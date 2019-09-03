e1859a8c753e5c9e854f382eaf2fe33c205934b6
Woohee Yang <woohee9527@gmail.com>
2019-09-03 14:36:56 +0900

    2차 수정
    1) scaling factor
    2) gammar, csi 수정
    3) bokeh 추가

--- DHMM/DHMM.py
+++ DHMM/DHMM.py
@@ -1,3 +1,4 @@
+# ~~~
 import numpy as np
 import matplotlib.pyplot as plt
 import pylab
@@ -6,10 +7,11 @@ import seaborn as sns
 import math
 import time, os
 import codecs, json
+import bokeh.plotting as bp
 
 class HMM:
     def __init__(self, N=0, M=0, data=None, load=False, params_file=None):
-        self.save_path = './'
+        self.save_path = './results/'
         folder = 'result_' + time.strftime('%Y-%m-%d-%H%M%S')
         self.save_path += folder + '/'
 
@@ -29,19 +31,8 @@ class HMM:
 
         self.visualization_training_result()
         # for Q function
-        self._alpha = np.zeros((self.T, self.N))
-        self._beta = np.zeros((self.T, self.N))
-        self._sca = np.zeros(self.T)
-        self._scb = np.zeros(self.T)
-
-        self._gammar = np.zeros((self.T, self.N))
-        self._csi = np.zeros((self.T, self.N, self.N))
+        self._init_probs()
         self.ll = 0.0
-
-        # for decoding : viterbi
-        self._delta = np.zeros(self.T)
-        self._psi = np.zeros(self.T)
-
 
     def _init_random_prob(self, n, m):
         tmp = np.random.rand(n, m)
@@ -54,6 +45,7 @@ class HMM:
         self._csi = np.zeros((self.T, self.N, self.N))
         self._alpha = np.zeros((self.T, self.N))
         self._beta = np.zeros((self.T, self.N))
+        self._ct = np.zeros(self.T)
         return
 
     def _forward_pass(self):
@@ -64,10 +56,10 @@ class HMM:
                 else:
                     self._alpha[t][i] = self.B[i][self.data[t]]
                     self._alpha[t][i] *= np.sum(self._alpha[t-1] * self.A.T[i])
-            # nomalization
+            # scaling = nomalization
             denom = np.sum(self._alpha[t], axis = 0)
             self._alpha[t] /= denom
-            self._sca[t] += 1/denom
+            self._ct[t] += denom
         return
 
     def _backward_pass(self):
@@ -78,32 +70,24 @@ class HMM:
                 else:
                     for j in range(self.N):
                         self._beta[t][i] += self._beta[t-1][j] * self.A[i][j] * self.B[j][self.data[self.T-t]]
-            # normalization
-            denom = np.sum(self._beta[t], axis = 0)
-            self._beta[t] /= denom
-            self._scb[t] += 1/denom
+            # scaling
+            self._beta[t] /= self._ct[t]
         return
 
     def _calc_gammar(self):
-        self._gammar[0] += self._alpha[0] * self._beta[self.T-1]
-        for t in range(1,self.T):
+        for t in range(self.T):
             for i in range(self.N):
-                self._gammar[t][i] = self._alpha[t][i] * self._beta[self.T-t][i]
-            # normalization
-            denom = np.sum(self._gammar[t], axis=0)
-            self._gammar[t] /= denom
+                self._gammar[t][i] = (self._ct[t]*self._alpha[t][i])
+                self._gammar[t][i] *= (self._ct[self.T-t-1]*self._beta[self.T-1-t][i])
         return
 
     def _calc_csi(self):
-        for t in range(self.T):
+        for t in range(1,self.T):
             denom = 0.0
             for i in range(self.N):
                 for j in range(self.N):
-                    self._csi[t][i][j] = self._alpha[t][i] * self.A[i][j]
-                    self._csi[t][i][j] *= self.B[j][self.data[t]] * self._beta[self.T-t-1][j]
-                # normalization
-                denom += np.sum(self._csi[t][i])
-            self._csi[t] /= denom
+                    self._csi[t][i][j] = (self._ct[t-1]*self._alpha[t-1][i]) * self.A[i][j]
+                    self._csi[t][i][j] *= self.B[j][self.data[t]] * (self._ct[self.T-1-t]*self._beta[self.T-1-t][j])
         return
 
     def _update_params(self):
@@ -137,7 +121,7 @@ class HMM:
                 self.B[i][j] = numer / denom[i]
         return
 
-    def train(self, max_iter = 100, tol = 0.0001):
+    def train(self, max_iter = 100, tol = 0.00001):
         for iter in range(max_iter):
             self._init_probs()
             # E step : Calculate the posteriors ; gammar, csi using alpha-beta
@@ -161,13 +145,14 @@ class HMM:
 
     def evaluate(self, num, tol):
         print('====================')
+        # print(self._ct)
         for t in range(self.T):
-            self._sca[t] = math.log(self._sca[t])
-        ll_ = np.sum(self._sca)
+            self._ct[t] = math.log(self._ct[t])
+        ll_ = np.sum(self._ct)
         ll_ = -ll_
         differ = abs(ll_ - self.ll)
         print('iteration #', num+1, ' / ll : ', ll_,' / differ : ', differ)
-        if differ <= tol :
+        if differ <= tol or ll_ < self.ll:
             return -1
         else:
             self.ll = ll_
@@ -175,6 +160,10 @@ class HMM:
         # return 0
 
     def decode(self):
+        # for decoding : viterbi
+        self._delta = np.zeros(self.T)
+        self._psi = np.zeros(self.T)
+
         delta = self.pi.reshape(1, -1) * self.B.T[self.data[0]]
         delta = delta.reshape(-1)
         # normalization
@@ -221,6 +210,10 @@ class HMM:
         sns.heatmap(self.B, annot=True, fmt='.5f', linewidth=6, cmap='Blues')
         self._figures.append(fig)
         if show: plt.show()
+        # plot = bp.figure(plot_width=300, plot_height=300)
+        # plot.square(x=[20,20,20,40], y=[20,20,40,40], size=[10,20,30,40], color="#74ADD1",fill_color=None)
+        # bp.show(plot)
+        return
 
     def visualization_decoding_result(self, show=False):
         fig = plt.figure()
@@ -229,19 +222,47 @@ class HMM:
         plt.plot(X, self._psi)
         plt.scatter(X, self._psi)
         for t in range(T):
-            plt.text(t+0.01, self._psi[t], round(self._delta[t], 5), fontsize=10)
+            plt.text(t+0.01, self._psi[t], round(self._delta[t],3), fontsize=8)
         self._figures.append(fig)
-        if show : plt.show()
+
+        p = bp.figure(
+            title='Viterbi result',
+            x_range=(-1,self.T+1),
+            y_range=(-0.5,self.N-1+0.5),
+            x_axis_type='linear',
+            tools = 'pan, wheel_zoom, box_zoom, reset, previewsave'
+        )
+        p.line(
+            X,
+            self._psi,
+            color = '#0066cc',
+            legend = 'state'
+        )
+        p.scatter(
+            X,
+            self._psi,
+            color = '#000000',
+        )
+        bp.output_file(filename=self.save_path+'bokeh_fig.html')
+        bp.save(p)
+        if show :
+            bp.show(p)
+            plt.show()
         return
 
     def save_params(self):
         os.mkdir(self.save_path)
-        file = 'params.json'
+        file = str(self.N)+'-'+str(self.T)+'-params.json'
 
         params = {
         'initial_probs' : self.pi.tolist(),
              'transition_matrix' : self.A.tolist(),
-             'emission_matrix' : self.B.tolist()
+             'emission_matrix' : self.B.tolist(),
+             'alpha : ' : self._alpha.tolist(),
+             'beta : ' : self._beta.tolist(),
+             'scaling factor : ' : self._ct.tolist(),
+             'gammar : ' : self._gammar.tolist(),
+             'csi : ' : self._csi.tolist()
         }
         jsonString = json.dumps(params)
 
@@ -262,9 +283,10 @@ if __name__ == "__main__":
     with open('cointossdata.txt','r') as fp:
         line = fp.readline()
     data = [int(i) for i in line]
-    hmm = HMM(2,2,data[:50])
+    hmm = HMM(2,2,data[:100])
     hmm.train()
     hmm.decode()
-    hmm.visualization(True)
+    hmm.visualization(False)
     # hmm = HMM(2, 2, data, True, './result_2019-08-30-1756/params.json')
     # HMM(2,2,data).load_params('./result_2019-08-30-1756/params.json')
+# ~~~
